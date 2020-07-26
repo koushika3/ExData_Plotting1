fd79bc9b33b32f696c9ad1d3a849820d0f3c50b0
koushika3 <koushika1234@gmail.com>
2020-07-27 00:15:11 +0530

    Plot1 copied

--- plot1.R
+++ plot1.R
@@ -5,11 +5,16 @@ unzip(zipfile = zipF, exdir = outDir)
 powerConsumption <- read.table("powerConsumption/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", stringsAsFactors = FALSE)
 powerConsumption$Date <- as.Date(powerConsumption$Date, "%d/%m/%Y")
 
+powerConsumption <- powerConsumption[complete.cases(powerConsumption),]
+
 library(dplyr)
 powerConsumption <- powerConsumption %>% filter(between(Date, as.Date("2007-02-01"), as.Date("2007-02-02")))
 
-plot(powerConsumption$Global_active_power)
+png("plot1.png", width=480, height=480)
 hist(powerConsumption$Global_active_power,
      main = "Global Active Power",
      xlab = "Global Active Power (kilowatts)",
      col = "red")
+dev.off()
+
+
