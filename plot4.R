d5416840c3b9de965b41085e0f4b382dd2aaba8b
koushika3 <koushika1234@gmail.com>
2020-07-27 01:23:55 +0530

    Plot 4 ready!

--- plot4.R
+++ plot4.R
@@ -10,16 +10,18 @@ powerConsumption <- powerConsumption[complete.cases(powerConsumption),]
 library(dplyr)
 powerConsumption <- powerConsumption %>% filter(between(Date, as.Date("2007-02-01"), as.Date("2007-02-02")))
 
-dateTime <- strptime(paste(as.character(powerConsumption$Date), powerConsumption$Time, sep=" "), "%Y-%m-%d %H:%M:%S") 
+datetime <- strptime(paste(as.character(powerConsumption$Date), powerConsumption$Time, sep=" "), "%Y-%m-%d %H:%M:%S") 
 globalActivePower <- powerConsumption$Global_active_power
 
-png("plot3.png", width=480, height=480)
-
+par(mfrow = c(2,2))
+png("plot4.png", width=480, height=480)
 with(powerConsumption, {
-     plot(dateTime, Sub_metering_1, ylab = "Global Active Power (kilowatts)",
-                            xlab = "", type = "l")
-     lines(dateTime, Sub_metering_2, col='Red')
-     lines(dateTime, Sub_metering_3, col='Blue')
-     })
-
+  plot(datetime, Global_active_power, type = "l")
+  plot(datetime, Voltage, type = "l", xlab = "")
+  plot(dateTime, Sub_metering_1, ylab = "Global Active Power (kilowatts)",
+       xlab = "", type = "l")
+  lines(dateTime, Sub_metering_2, col='Red')
+  lines(dateTime, Sub_metering_3, col='Blue')
+  plot(datetime, Global_reactive_power, type = "l", ylab = "Global Reactive Power (kilowatts)")
+})
 dev.off()
